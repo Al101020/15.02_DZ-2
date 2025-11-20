5e36d30b8073eb3f977f30e43d3cf48e8f4a952b
Al101020 <alex.vl.m@yandex.ru>
2025-11-20 16:50:39 +1000

    commit-5

--- src/component/store/Store.jsx
+++ src/component/store/Store.jsx
@@ -1,49 +1,80 @@
 import React, {useState} from 'react';
 import './Store.css';
+// import products from './products.jsx';
+
+// function ViewList({ items }) {
+function ViewList() {
+  let index = 0;
+  console.log(index + ' - ViewList');
+
+  // return (
+  //   <div className='ViewList'>
+  //     {items.map((item) => (
+  //       <ShopItem item={ item } key={ index + 1 } />
+  //     ))}
+  //   </div>
+  // );
+};
+
+// function ViewModules({ cards }) {
+function ViewModules() {
+  let index = 0;
+  console.log(index + ' - ViewModules');
+
+  // return (
+  //   <div className="ViewModules">
+  //     {cards.map((card) => (
+  //       <ShopCard card={ card } key={ index + 1 } />
+  //     ))}
+  //   </div>
+  // );
+};
+
+
+
+
 
 const IconSwitch = ({ icon, onSwitch }) => {
   return (
-    <div className='icon' onClick={onSwitch} style={{ cursor: 'pointer' }}>
-      <span className="material-icons">
-        {icon ? 'view_list' : 'view_module'}
-      </span>
+    <div className='iCon material-icons' onClick={onSwitch}>
+      {icon}
     </div>
   );
-};
+}
 
 function Store() {
-  const [listView, setListView] = useState(true);
+  const [icon, setListView] = useState('view_list');
 
-  const switchingView = () => {
-    // view_list(!view_module) 
-    // подсмотрел было так:const toggleView = () => {setIsListView(!isListView);};
+  const switchingViews = () => {
+    const iccon = document.querySelector('.iCon');
+    // const listProducts = document.querySelector('.list-products');
 
-    const aaa = document.querySelector('.icon');
-    // aaa.classList.add('iconList')
-    console.log(aaa.className);
+    if (icon === 'view_list') {
+      iccon.className='iCon iconModule';
+      return setListView('view_module');
+    } else if (icon === 'view_module') {
+      iccon.className='iCon material-icons';
+      return setListView('view_list');
+    };
   };
   
   return (
     <>
-      <div className="Task-2">
-        <h3 className="title-2">Task-2</h3>
-        <IconSwitch icon={"view_list"} onSwitch={switchingView}/>
-        <div className="list-products"></div>
+      <div className='Task-2'>
+        <h3 className='title-2'>Task-2</h3>
+        <IconSwitch icon={'view_list'} onSwitch={switchingViews}/>
+        <div className='list-products'>
+          {icon === "view_list" ? (
+            // <ViewModules cards={products} />
+            <ViewModules />
+          ) : (
+            // <ViewList items={products} />
+            <ViewList />
+          )}
+        </div>
       </div>
     </>
   );
 }
 
-export default Store
-
-// const IconSwitch = ({ icon, onSwitch }) => {
-//   return (
-//     <div className='icon' onClick={onSwitch} style={{ cursor: 'pointer' }}>
-//         <span className="material-icons">
-//             {icon ? 'view_list' : 'view_module'}
-//         </span>
-//     </div>
-//   );
-// };
-
-// export default IconSwitch;
+export default Store;
